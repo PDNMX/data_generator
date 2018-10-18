e3b42d83f20c285e6421343f18a2701c1156cdf3
Mario Torres <mario291187@gmail.com>
2018-10-18 10:28:05 -0500

    CLI option parser

--- datagen.py
+++ datagen.py
@@ -1,1 +1,22 @@
-# data generator 
+# data generator
+from pprint import pprint
+import pandas as pd
+import json
+import argparse
+
+
+parser = argparse.ArgumentParser(description='SESNA data generator')
+parser.add_argument('-s','--sys', default=0, type=int,  help='System number', choices=[1,2,3])
+parser.add_argument('-o','--out', default='JSON', help='Output format', choices= ['JSON', 'CSV'])
+args = parser.parse_args()
+
+sys_number = args.sys
+
+if sys_number == 1:
+    print ('Sistema 1 -> Declaraciones ')
+elif sys_number == 1:
+    print ('Sistema 2 -> Servidores públicos que intervienen en contrataciones')
+elif sys_number == 2:
+    print ('Sistema 3 -> Servidores públicos y particulares sancionados')
+else:
+    parser.print_help()
