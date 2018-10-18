874fed60818b3880b48a1e6eacb58273e4fd6c48
Mario Torres <mario291187@gmail.com>
2018-10-18 10:45:05 -0500

    CLI option parser

--- datagen.py
+++ datagen.py
@@ -6,14 +6,18 @@ import argparse
 
 
 parser = argparse.ArgumentParser(description='SESNA data generator')
-parser.add_argument('-s','--sys', default=0, type=int,  help='System number', choices=[1,2,3])
+parser.add_argument('-s','--sys', default=0, type=int,  help='System number', choices=[1, 2, 3])
+parser.add_argument('-n', '--samples', default=10, type=int, help='Number of samples')
 parser.add_argument('-o','--out', default='JSON', help='Output format', choices= ['JSON', 'CSV'])
 args = parser.parse_args()
 
 sys_number = args.sys
+samples = args.samples
 
 if sys_number == 1:
     print ('Sistema 1 -> Declaraciones ')
+    for x in range(0, samples):
+        print('dec')
 elif sys_number == 1:
     print ('Sistema 2 -> Servidores públicos que intervienen en contrataciones')
 elif sys_number == 2:
