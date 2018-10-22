9c9e7af461845e2afabf04001331ec18607b69ef
Mario Torres <mario291187@gmail.com>
2018-10-21 21:23:04 -0500

    ...

--- datagen.py
+++ datagen.py
@@ -5,6 +5,7 @@ import json
 import argparse
 #import sqlite3
 import uuid
+import random
 
 parser = argparse.ArgumentParser(description='SESNA data generator')
 parser.add_argument('-s','--sys', default=0, type=int,  help='System number', choices=[1, 2, 3])
@@ -15,6 +16,12 @@ args = parser.parse_args()
 sys_number = args.sys
 number_of_samples = args.samples
 
+def get_email(nombre, primer_apellido):
+    return "{0}.{1}@example.com".format(nombre,primer_apellido)
+
+def get_telephone():
+    return str(random.randint(5500000000,7779999999))
+
 if sys_number == 1:
     print ('Sistema 1 -> Declaraciones ')
     samples = []
@@ -23,6 +30,7 @@ if sys_number == 1:
     hombres = pd.read_csv('./corpus/hombres.csv')
     mujeres = pd.read_csv('./corpus/mujeres.csv')
     apellidos = pd.read_csv('./corpus/apellidos-20.csv')
+    # domicilios
     # conn = sqlite3.connect('corpus.db')
 
     for x in range(0, number_of_samples):
@@ -30,9 +38,9 @@ if sys_number == 1:
         # información personal
         sample['informacion_personal'] = {}
         sample['informacion_personal']['id'] = str(uuid.uuid1())
-        sample['informacion_personal']['nombres'] = ''
-        sample['informacion_personal']['primer_apellido'] = ''
-        sample['informacion_personal']['segundo_apellido'] = ''
+        sample['informacion_personal']['nombres'] = 'Juan'
+        sample['informacion_personal']['primer_apellido'] = 'Pérez'
+        sample['informacion_personal']['segundo_apellido'] = 'García'
         sample['informacion_personal']['nacionalidad_representante'] = {}
         sample['informacion_personal']['nacionalidad_representante']['pais'] = 'México'
         sample['informacion_personal']['nacionalidad_representante']['codigo'] = 'MX'
@@ -48,15 +56,28 @@ if sys_number == 1:
         sample['informacion_personal']['correo_electronico'] = {}
         sample['informacion_personal']['correo_electronico']['laboral'] = ''
         sample['informacion_personal']['correo_electronico']['personal'] = ''
-        sample['informacion_personal']['telefono']={}
-        sample['informacion_personal']['telefono']['laboral']={}
-        sample['informacion_personal']['telefono']['personal']= ''
-        sample['informacion_personal']['telefono']['celular']= ''
+        sample['informacion_personal']['telefono'] = {}
+        sample['informacion_personal']['telefono']['laboral']= get_telephone()
+        sample['informacion_personal']['telefono']['personal']= get_telephone()
+        sample['informacion_personal']['telefono']['celular']= get_telephone()
         sample['informacion_personal']['domicilio'] = {}
+        sample['informacion_personal']['domicilio']['pais'] = 'MX'
+        sample['informacion_personal']['domicilio']['entidad_federativa'] = {}
+        sample['informacion_personal']['domicilio']['entidad_federativa']['nom_ent'] = 'México'
+        sample['informacion_personal']['domicilio']['entidad_federativa']['cve_ent'] = '15'
+        sample['informacion_personal']['domicilio']['municipio']= {}
+        sample['informacion_personal']['domicilio']['municipio']['nom_mun']= 'Ecatepec de Morelos'
+        sample['informacion_personal']['domicilio']['municipio']['cve_mun']= '033'
+        sample['informacion_personal']['domicilio']['cp']='55018'
+        sample['informacion_personal']['domicilio']['localidad']={}
+        sample['informacion_personal']['domicilio']['localidad']['nom_loc']='Ecatepec de Morelos'
+        sample['informacion_personal']['domicilio']['localidad']['cve_loc'] = '001'
+        sample['informacion_personal']['domicilio']['vialidad']={}
+        sample['informacion_personal']['domicilio']['vialidad']['tipo_vial']= 'CALLE'
+        sample['informacion_personal']['domicilio']['vialidad']['nom_vial']='El Rosal'
 
 
-
-        sample['informacion_personal']['datos_curriculares'] = {}
+        sample['informacion_personal']['datos_curriculares']={}
         sample['informacion_personal']['datos_curriculares']['grado_maximo_escolaridad'] = ''
         # Intereses
         sample['intereses'] = {}
@@ -80,3 +101,6 @@ elif sys_number == 2:
     print ('Sistema 3 -> Servidores públicos y particulares sancionados')
 else:
     parser.print_help()
+
+
+
